dbb46859ad2edaf93791007750b6030419ffbe42
michaelaeron33-cyber <michaelaeron33@gmail.com>
2026-02-04 10:36:48 +0800

    "add_fallback_route"

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/students" element={<Students />} />
+            <Route path="*" element={<Home />} />
           </Routes>
         </main>
         <footer className="app-footer">
